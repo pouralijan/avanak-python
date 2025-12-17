db2a59729ae350ac452c2f1c4afbe51c6b6dafc9
Hassan Pouralijan <pouralijan@gmail.com>
2025-12-17 09:45:51 +0330

    feat: Add cli tool.

--- src/avanak/__init__.py
+++ src/avanak/__init__.py
@@ -1,6 +1,7 @@
 """Avanak API Python Client"""
 
+from .cli import cli
 from .client import AvanakClient
 
-__version__ = "0.1.1"
-__all__ = ["AvanakClient"]
+__version__ = "0.1.2"
+__all__ = ["AvanakClient", "cli"]
